85cca8e6d9ef3adbc53cf3dcb01dc3e6b0f31adf
Cheryl Ohashi <cheryl.ohashi@gmail.com>
2016-05-01 08:49:53 -0700

    Added recommendation field and used some bootstrap to line up the form

--- mc_site/BTdUbs/forms.py
+++ mc_site/BTdUbs/forms.py
@@ -5,3 +5,6 @@ class StoreForm(forms.ModelForm):
     class Meta:
         model = Store
         fields = ('store_name', 'url',)
+
+class timeSlotForm(forms.Form):
+    order_time = forms.TimeField(widget=forms.TimeInput(format='%h:%M'))
