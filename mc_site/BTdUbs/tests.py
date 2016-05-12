669208efda9a2eb50c626e0f277f524a0ee1e43d
Cheryl Ohashi <cheryl.ohashi@gmail.com>
2016-05-11 20:43:49 -0700

    add background picture and started tests

--- mc_site/BTdUbs/tests.py
+++ mc_site/BTdUbs/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
+from BTdUbs.models import Store 
 # Create your tests here.
+
+class StoreTestCase(TestCase):
+    def setup(self):
+        Store.objects.create( store_name="another store", url="www.anotherstore.com")
+
+    def test_unicode(self):
+        expected = "a store"
+        s1 = Store(store_name=expected)
+        self.assertEqual(expected, s1.store_name)
+
+    def test_url_return(self):
+        expected = "http://www.astore.com"
+        s1=Store(store_name="a store", url=expected)
+        self.assertEqual(s1.url, expected)
