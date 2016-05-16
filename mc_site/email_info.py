20332133a4804cc4c95319281a9aa2f370b62d95
Michelle Yu <michellehyu7@gmail.com>
2016-05-15 21:36:51 -0700

    configure for mail to use sendgrid

--- mc_site/email_info.py
+++ mc_site/email_info.py
@@ -1,6 +1,7 @@
-# for gmail
+import os
+# for sendgrid
 EMAIL_USE_TLS= True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER= 'youremail.gmail.com'
-EMAIL_HOST_PASSWORD = 'yourpasssword'
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER= os.environ['SENDGRID_USERNAME']
+EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
 EMAIL_PORT = 587
