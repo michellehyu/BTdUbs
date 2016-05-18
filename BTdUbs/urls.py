84deafc35614038e5d9200676f653f2ac7343871
Cheryl Ohashi <cheryl.ohashi@gmail.com>
2016-05-17 20:52:40 -0700

    save store and url to the database

--- BTdUbs/urls.py
+++ BTdUbs/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^selection/$', views.selection, name='selection'),
-    url(r'^confirmation/$', views.confirmation, name='confirmation'),
     url(r'^$', views.welcome, name='welcome'),
 ]
--- BTdUbs/views.py
+++ BTdUbs/views.py
@@ -1,10 +1,13 @@
 from django.conf import settings
 from django.core.mail import send_mail
+from django.core.urlresolvers import reverse
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.template.loader import render_to_string
-from .forms import StoreForm
 from django.contrib.auth.models import User
+from .models import Store
+from .forms import StoreForm
 
 # Create your views here.
 # Default Welcome Page
@@ -17,34 +20,34 @@ def selection(request):
     if request.method == "POST":
         form = StoreForm(request.POST)
         if form.is_valid():
+            # save new stores/url to the database
             daily_selection = form.save(commit=False)
-            daily_selection.store_name = request.store_name
-            daily_selection.url = request.url
+            daily_selection.store_name = request.POST['store_name']
+            daily_selection.url = request.POST['url']
             daily_selection.save()
-            return HttpResponseRedirect(reverse())
+
+            # Send an email to all the users
+            subject = 'BuyTheWay Order Today'
+            user_name = request.user
+            context = {
+                'user': 'someone',
+                'store': request.POST['store_name'],
+                'name': user_name,
+                'url': request.POST['url'],
+                'recommendation': request.POST['recommendation'],
+                'order_time': request.POST['order_time'],
+                'note': request.POST['note']
+            }
+            message = render_to_string('BTdUbs/email/order_email.txt', context)
+            print(message)
+            from_email = User.objects.get(username=user_name).email
+            users = User.objects.all()   
+            to_list = [u.email for u in users if u.email != from_email]
+            send_mail(subject,message, from_email, to_list, fail_silently=True)
+            return HttpResponse("The members have been notified")
+
         else:
-            return HttpResponse("error")
+            return render(request, 'BTdUbs/selection.html', {'form': form})
     else:
         return render(request, 'BTdUbs/selection.html', {'form': form})
 
-# Confirmation Page for Buyer
-def confirmation(request):
-    subject = 'BuyTheWay Order Today'
-    user_name = request.user
-    context = {
-        'user': 'someone',
-        'store': request.POST['store_name'],
-        'name': user_name,
-        'url': request.POST['url'],
-        'recommendation': request.POST['recommendation'],
-        'order_time': request.POST['order_time'],
-        'note': request.POST['note']
-        }
-    message = render_to_string('BTdUbs/email/order_email.txt', context)
-    print(message)
-    from_email = User.objects.get(username=user_name).email
-    users = User.objects.all()   
-    to_list = [u.email for u in users if u.email != from_email]
-    send_mail(subject,message, from_email, to_list, fail_silently=True)
-    # send_mass_mail()
-    return HttpResponse("The members have been notified")
